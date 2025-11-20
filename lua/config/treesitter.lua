fc9c9379b714bfec16ffd8e80b8fdd460ae1058d
Dorian Boully <dorian@boully.fr>
2025-11-20 09:41:24 +0100

    autostart treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,2 +1,19 @@
-vim.treesitter.language.register("latex", "tex")
-vim.treesitter.language.register("latex", "plaintex")
+local Parsers = {
+  latex = { "tex", "plaintex" },
+  typst = "typst" ,
+  lua = "lua",
+  json = "json",
+  toml = "toml",
+  python = "python",
+}
+
+vim.iter(pairs(Parsers)):each(function(parser, filetypes)
+  vim.treesitter.language.register(parser, filetypes)
+
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = filetypes,
+    callback = function(ev)
+      vim.treesitter.start(ev.buf, parser)
+    end
+  })
+end)
