eeb1341e840fed2369f26948c9f1351d77a8b4e3
Dorian Boully <dorian@boully.fr>
2025-11-27 22:43:44 +0100

    autopairs rules for $ in typst

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -4,25 +4,26 @@ return {
   name = "nvim-autopairs",
 
   opts = {
-    extraRules = {
-      lua = { { "<", ">" } },
-      typst = { { "$", "$" } },
-      tex = { { "$", "$" } },
-    },
+    check_ts = true,
   },
 
   config = function(opts)
     local Rule = require('nvim-autopairs.rule')
     local autopairs = require("nvim-autopairs")
+    local cond = require("nvim-autopairs.conds")
 
     autopairs.setup()
 
-    vim.iter(pairs(opts.extraRules)):each(
-      function(fileType, rules)
-        vim.iter(rules):each(function(rule)
-          autopairs.add_rule(Rule(rule[1], rule[2], fileType))
-        end)
-      end
+    autopairs.add_rule(
+      Rule("$", "$", "typst")
+      :with_pair(cond.not_before_regex("\\", 1)) -- ignore escaped \$
+      :with_pair(function(o)
+        local _, count = o.line
+            :gsub("\\%$", "")
+            :gsub("%$", "")
+        return count % 2 == 0
+      end)
+      :with_move(cond.done())
     )
   end
 }
